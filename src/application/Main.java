d1ed2e27d126b05cdaa11d2e3e3370a1f16de2b8
Amanda <amandabarretoolima@gmail.com>
2025-12-09 09:51:27 -0300

    Implementando os metodos removeProducts e addProducts.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,41 @@
 package application;
+import java.util.Scanner;
+import java.util.Locale;
+import entities.Product;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter the name of the products: ");
+        String name = sc.nextLine();
+        System.out.println("Enter the price of the products: ");
+        double price = sc.nextDouble();
+        System.out.println("Enter the quantity in stock: ");
+        int quantity = sc.nextInt();
+
+        System.out.println("----MENU----");
+        System.out.println("1. Add Products");
+        System.out.println("2. Remove Products");
+        System.out.println("3. Look up Products");
+        System.out.println("4. Exit the program");
+        System.out.println("Choose one option: ");
+        int op = sc.nextInt();
+
+        switch (op) {
+            case 1:
+                System.out.println("Enter the number of products to be added in stock: ");
+                int addQuantity = sc.nextInt();
+                break;
+            case 2:
+                System.out.println("Enter the number of products to be removed from stock: ");
+                int removeQuantity = sc.nextInt();
+                break;
+            case 3:
+                System.out.println("Updated data: %s, $ %f, %d units, Total: %f",name,price,quantity,totalValueInStock);
+
+        }
     }
 }
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -6,4 +6,16 @@ public class Product {
     public double price;
     public int quantity;
 
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    public void addProducts (int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProducts (int quantity) {
+        this.quantity -= quantity;
+    }
+
 }
